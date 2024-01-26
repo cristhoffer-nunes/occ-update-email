28cf13deddaf7e7be469520a15d66043c9bfa1b4
Cristhoffer Santos <nunes-santos@hotmail.com>
2024-01-26 19:21:24 -0300

    feat: add config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OccModule } from './module/occ.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [OccModule],
+  imports: [OccModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  const logger = new Logger(bootstrap.name);
+  await app.listen(port, () => {
+    logger.log(`Service listening on port ${port}`);
+  });
 }
 bootstrap();
