337f07c1226a21987567f726742ee2f15f610ae1
Cristhoffer Santos <nunes-santos@hotmail.com>
2024-01-26 17:57:58 -0300

    refactor: remove config from node env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,18 +1,7 @@
 import 'dotenv/config';
 
-let url = '';
-let appKey = '';
-
-if (process.env.NODE_ENV === 'dev') {
-  url = process.env.DEV_URL;
-  appKey = process.env.DEV_APPKEY;
-} else if (process.env.NODE_ENV === 'tst') {
-  url = process.env.TST_URL;
-  appKey = process.env.TST_APPKEY;
-} else if (process.env.NODE_ENV === 'prd') {
-  url = process.env.PRD_URL;
-  appKey = process.env.PRD_APPKEY;
-}
+const url = process.env.OCC_URL;
+const appKey = process.env.OCC_APPKEY;
 
 export const envVariables = {
   url,
